b52ef26d5abc6429e692fb66043fa0867e40df50
Matt E <mathew.estrada@sjsu.edu>
2024-01-07 06:31:20 -0800

    Initial game set-up

---
+++ src/main/java/com/example/GamePanel.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+
+    public GamePanel() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.fillRect(100, 100, 200, 50);
+    }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Game is running successfully! c:");
+        new Game();
     }
 }
