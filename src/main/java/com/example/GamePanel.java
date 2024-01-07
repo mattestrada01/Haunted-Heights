c40d8b45a0d58666599c655ba90ff84986cb28db
Matt E <mathew.estrada@sjsu.edu>
2024-01-07 09:17:56 -0800

    User's character imported and can now be moved with keyboard or mouse

--- src/main/java/com/example/GamePanel.java
+++ src/main/java/com/example/GamePanel.java
@@ -1,9 +1,12 @@
 package com.example;
 
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.Random;
-
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import com.example.inputs.KeyboardInputs;
 import com.example.inputs.MouseInputs;
@@ -12,20 +15,35 @@ public class GamePanel extends JPanel{
 
     private MouseInputs mouseInputs;
     private float xDelta = 100, yDelta = 100;
-    private float xDirection = 1f, yDirection = 1f;
-    private int frameCount = 0;
-    private long lastChecked = 0;
-    private Color color = Color.PINK;
-    private Random random;
+    private BufferedImage image, subImage;
+
 
     public GamePanel() {
-        random = new Random();
         mouseInputs = new MouseInputs(this);
+        importImage();
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
         setFocusable(true);
         requestFocusInWindow();
+    }
+
+    private void importImage() {
+        File is = new File("src/main/resources/player_sprites.png");
+    
+        try {
+            image = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setPanelSize() {
+        Dimension dimension = new Dimension(1280, 800);
+        setMinimumSize(dimension);
+        setMaximumSize(dimension);
+        setPreferredSize(dimension);
     }
 
     public void changeXDelta(int value) {
@@ -43,31 +61,7 @@ public class GamePanel extends JPanel{
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        updateRectangle();
-        g.setColor(color);
-
-        g.fillRect((int)xDelta, (int)yDelta, 200, 45);
-    }
-
-    private void updateRectangle() {
-        xDelta+=xDirection;
-        if (xDelta > 400 || xDelta < 0) {
-            xDirection *= -1;
-            color = getRandomColor();
-        }
-        
-        yDelta+=yDirection;
-        if (yDelta > 400 || yDelta < 0) {
-            yDirection *= -1;
-            color = getRandomColor();
-        }
-    }
-
-    private Color getRandomColor() {
-        int r = random.nextInt(255);
-        int g = random.nextInt(255);
-        int b = random.nextInt(255);
-        return new Color(r,g,b);
+        subImage = image.getSubimage(1*64, 8*40, 64, 40);
+        g.drawImage(subImage, (int)xDelta, (int)yDelta, 128, 80, null);
     }
 }
